a5038f75d1eb5228a16827a51a4dec78298aa07f
Zion Opaaje <zopaaje8@gmail.com>
2026-09-20 11:40:50 +0100

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type { MetadataRoute } from "next";
+export default function sitemap(): MetadataRoute.Sitemap { const url="https://zionopaaje.name.ng"; return [{url,lastModified:new Date()}]; }
