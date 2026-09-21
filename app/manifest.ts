f2607e020a82448d421021044df40c3491be41f0
Zion Opaaje <zopaaje8@gmail.com>
2026-09-21 10:12:36 +0100

    Fix manifest favicon path

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#111111",
     icons: [
       {
-        src: "/favicon.png",
+        src: "/favicon%20(1).png",
         type: "image/png",
         sizes: "any",
         purpose: "any"
